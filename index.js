2a3072aad9262d3ecc13e36a0d6054b8ea6d1e00
Aryan Sharma <72523773+aryn117@users.noreply.github.com>
2022-12-01 10:37:44 +0530

    fix

--- index.js
+++ index.js
@@ -36,18 +36,23 @@ const scrapeData = async (slug = "2d408aa1ffeaecb648de1d0ea0f633496fccc5d85b54e5
   }
 };
 
-app.get("/", (req, res) => {
-  res.send('JIZZ');
-})
-
-app.get("/get/:id", async (req, res) => {
-
-  console.log(req.params.id);
-
+app.get("/:id", async (req, res) => {
+  // res.send('JIZZ');
   const return_data = await scrapeData(req.params.id);
   res.send(return_data);
-});
+})
+
+// app.get("/get/:id", async (req, res) => {
+
+//   console.log(req.params.id);
+//   res.send(`/get: ${req.params.id}`);
+
+
+// });
 
 app.listen(app.get('port'), () => {
   console.log(`Running at: ${app.get('port')}`);
 });
+
+
+// https://weather.com/en-IN/weather/today/l/7b837a91d2a8d74a54eeb9353148c24b8a4b54e8df9e51607f695cb76d062587
